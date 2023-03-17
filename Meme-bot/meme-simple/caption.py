bd7da39b04e4ebc600014d615e7cf2dbee1f44a8
hiresh-poosarla <70046079+hiresh-poosarla@users.noreply.github.com>
2023-03-17 15:58:20 -0700

    Update caption.py

--- Meme-bot/meme-simple/caption.py
+++ Meme-bot/meme-simple/caption.py
@@ -1,53 +1,28 @@
-import os, discord
-from discord.ext import commands
-import mimetypes
-import requests
+from PIL import Image, ImageDraw, ImageFont
 from io import BytesIO
-from caption import caption_image
+import textwrap
 
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-SUPPORTED_MIMETYPES = ["image/jpeg", "image/png", "image/webp"]
+def caption_image(image_file, caption, font="impact.ttf"):
+    img = Image.open(image_file)
+    draw = ImageDraw.Draw(img)
 
-bot = commands.Bot(command_prefix="!")
+    font_size = int(img.width/16)
+    font = ImageFont.truetype("impact.ttf", font_size)
 
-@bot.event
-async def on_ready():
-    print(f"{bot.user} has connected to Discord!")
+    caption = textwrap.fill(text=caption, width=img.width/(font_size/2))
 
-@bot.command(name="caption", brief="Add a caption to an image.", help="""Add a caption to an attached image. Example:
-             
-!caption "Hello world!" <attached image>
-
-Supported image types: PNG, JPEG, WebP
-             """)
-async def caption(ctx, caption_text):
-    # Must have caption text
-    if not caption_text:
-        await ctx.message.reply("Please include some caption text after the `!caption` command. For example `!caption \"Hello world!\"")
-        return
+    caption_w, caption_h = draw.textsize(caption, font=font)
     
-    # Must have a file attached
-    if ctx.message.attachments:
-        image_url = ctx.message.attachments[0].url
-    else:
-        await ctx.message.reply("Please attach an image for me to caption.")
-        return
+    draw.text(((img.width-caption_w)/2, (img.height-caption_h)/8), # position
+              caption, # text
+              (255,255,255), # color
+              font=font, # font
+              stroke_width=2, # text outline width
+              stroke_fill=(0,0,0)) # text outline color
 
-    # File must be an image
-    if mimetypes.guess_type(image_url)[0] not in SUPPORTED_MIMETYPES:
-        await ctx.message.reply("Sorry, the file you attached is not a supported image format. Please upload a PNG, JPEG or WebP image.")
-        return
+    with BytesIO() as img_bytes:
+        img.save(img_bytes, format=img.format)
+        content = img_bytes.getvalue()
+    
+    return content
 
-    # Fetch image file
-    response = requests.get(image_url)
-
-    # Store image file name
-    image_filename = ctx.message.attachments[0].filename
-
-    # Caption image
-    final_image = caption_image(BytesIO(response.content), caption_text)
-
-        # Send reply
-    await ctx.message.reply(file=discord.File(BytesIO(final_image), filename=f"captioned-{image_filename}"))
-
-bot.run(DISCORD_TOKEN)
